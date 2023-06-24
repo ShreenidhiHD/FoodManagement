521d5b210fea7604bdf290f9b1b35af470f96463
Shreenidhi H D <shreenidhihaagalagodu@gmail.com>
2023-06-25 00:44:25 +0530

    Updated login
    Login was working fine
    
    note still need to remove console printings
    it was printing tokens

--- food-laravel/app/Http/Controllers/UserController.php
+++ food-laravel/app/Http/Controllers/UserController.php
@@ -1,12 +1,49 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\ValidationException;
+use Illuminate\Support\Facades\Log;
 
 class UserController extends Controller
 {
-    public function getUser() {
-        return response()->json(['name' => 'John Doe']);
-    }
+    public function signup(Request $request)
+        {
+            try {
+                $validatedData = $request->validate([
+                    'name' => 'required',
+                    'email' => 'required|email|unique:users',
+                    'password' => 'required|min:8',
+                ]);
+
+                // Logging statements
+                Log::info('Validated Data:', $validatedData);
+
+                $validatedData['password'] = Hash::make($request->password);
+
+                $user = User::create($validatedData);
+
+                // Create a token for the user
+                $token = $user->createToken('authToken')->plainTextToken;
+
+                // Logging statements
+                Log::info('User created:', [$user->toArray()]);
+
+                return response()->json(['token' => $token], 201);
+            } catch (ValidationException $e) {
+                // Logging statement
+                Log::error('Validation error:', $e->errors());
+
+                return response()->json(['message' => $e->errors()], 422);
+            } catch (\Exception $e) {
+                // Logging statement
+                Log::error('Exception:', $e->getMessage());
+
+                return response()->json(['message' => 'Signup failed. Please try again.'], 500);
+            }
+        }
+
 }
+
--- food-laravel/routes/api.php
+++ food-laravel/routes/api.php
@@ -17,9 +17,8 @@ use App\Http\Controllers\SettingsController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+
+Route::post('/signup', [UserController::class, 'signup']);
 
 Route::get('/user', [UserController::class, 'getUser']);
 
