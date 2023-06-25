da1d58285f305fc225193e3350efda3bc1330f28
Shreenidhi H D <shreenidhihaagalagodu@gmail.com>
2023-06-25 18:11:59 +0530

    updated navbar for logged  user

--- food-react/src/components/Navbar.js
+++ food-react/src/components/Navbar.js
@@ -2,16 +2,9 @@ import React, { useContext } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
 import Typography from '@mui/material/Typography';
-import Menu from '@mui/material/Menu';
-import MenuIcon from '@mui/icons-material/Menu';
 import Container from '@mui/material/Container';
-import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
-import Tooltip from '@mui/material/Tooltip';
-import MenuItem from '@mui/material/MenuItem';
-import AdbIcon from '@mui/icons-material/Adb';
 import { Link as RouterLink } from 'react-router-dom';
 import { SettingsContext } from '../server/SettingsProvider';
 
@@ -22,23 +15,14 @@ const pages = [
 ];
 
 function ResponsiveAppBar() {
-  const [anchorElNav, setAnchorElNav] = React.useState(null);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
   const settings = useContext(SettingsContext);
 
-  const handleOpenNavMenu = (event) => {
-    setAnchorElNav(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+  const isAuthenticated = localStorage.getItem('authToken') !== null;
 
   return (
     <AppBar position="sticky" sx={{ backgroundColor: '#f8f8f8', color: '#333' }}>
       <Container maxWidth="xl">
         <Toolbar disableGutters>
-         
           <Typography
             variant="h6"
             noWrap
@@ -67,12 +51,28 @@ function ResponsiveAppBar() {
               </Button>
             ))}
           </Box>
-          <Button component={RouterLink} to="/login" color="inherit">
-            Login
-          </Button>
-          <Button component={RouterLink} to="/signup" color="inherit">
-            Signup
-          </Button>
+          {isAuthenticated ? (
+            <>
+              <Button component={RouterLink} to="/profile" color="inherit">
+                Profile
+              </Button>
+              <Button onClick={() => {
+                localStorage.removeItem('authToken');
+                // Add a redirect to the home page here after logging out
+              }} color="inherit">
+                Logout
+              </Button>
+            </>
+          ) : (
+            <>
+              <Button component={RouterLink} to="/login" color="inherit">
+                Login
+              </Button>
+              <Button component={RouterLink} to="/signup" color="inherit">
+                Signup
+              </Button>
+            </>
+          )}
         </Toolbar>
       </Container>
     </AppBar>
