2e29d7df5ebdcdc7095a7192926d5d249d9f6f26
Shreenidhi H D <shreenidhihaagalagodu@gmail.com>
2023-06-25 19:49:42 +0530

    added browsemore button

---
+++ food-react/src/components/BrowseMore.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link as RouterLink } from 'react-router-dom';
+import Button from '@mui/material/Button';
+
+const BrowseMore = ({ isAuthenticated }) => {
+  const buttonStyle = {
+    marginTop: '50px',
+  };
+
+  if (isAuthenticated) {
+    // User is logged in, show the browse more button with blue color
+    return (
+      <Button
+        component={RouterLink}
+        to="/browse"
+        variant="contained"
+        color="primary"
+        style={buttonStyle}
+      >
+        Browse More
+      </Button>
+    );
+  } else {
+    // User is not logged in, show the login link with pink color
+    return (
+      <Button
+        component={RouterLink}
+        to="/login"
+        variant="contained"
+        color="secondary"
+        style={buttonStyle}
+      >
+        Login to Browse More
+      </Button>
+    );
+  }
+};
+
+export default BrowseMore;
