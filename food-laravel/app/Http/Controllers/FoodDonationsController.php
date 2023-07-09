b6a98dc2be31507ac1646d1f3be709a7cf46d674
pramod-rkp <99354649+pramod-rkp@users.noreply.github.com>
2023-07-09 20:05:58 +0530

    Updated donations controller
    Public display api updated and functioning as expected

--- food-laravel/app/Http/Controllers/FoodDonationsController.php
+++ food-laravel/app/Http/Controllers/FoodDonationsController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Log;
 use Illuminate\Database\QueryException;
 use App\Helpers\EmailHelper;
 use PDOException;
+use DB;
 
 class FoodDonationsController extends Controller
 {
@@ -94,15 +95,13 @@ class FoodDonationsController extends Controller
     public function get_user_details_by_userid($id){
         $user_array=Array();
         //Fetch from users table
-        $users = DB::table('users')->find($id);
+        $users = DB::table('users')->where('id',$id)->get();;
         foreach($users as $user){
-            if($user['status']!='deactived'){
-                //Assigning to temprovary array
-                $temp=Array();
-                $temp['user_id']=$user['id'];
-                $temp['user_name']=$user['name'];
-                ($user['status']=='verified')?$temp['is_verified']=true:$temp['is_verified']=false;
-                array_push($user_array,$temp);
+            if($user->status!='deactived'){
+                //Assigning to array
+                $user_array['user_id']=$user->id;
+                $user_array['user_name']=$user->name;
+                ($user->status=='verified')?$user_array['is_verified']=true:$user_array['is_verified']=false;
             }
         }
         return $user_array;
@@ -111,13 +110,40 @@ class FoodDonationsController extends Controller
     //Returns all active donation list
     public function donation_list()
     {
-        
-        $donation = Donation::get();
-        if (!$donation) {
+        $donations_list=Array();
+        $donations = Donation::get();
+        foreach($donations as $donation){
+            $temp=Array();
+            //Get user details
+            $user=$this->get_user_details_by_userid($donation['Created_by']);
+            if(is_array($user) and count($user)>0){
+                $temp['userid']=$user['user_id'];
+                $temp['username']=$user['user_name'];
+                $dontempations_list['verified']=$user['is_verified'];
+            }
+            $temp['id']=$donation['id'];
+            $temp['number_of_plates']=$donation['number_of_plates'];
+            $temp['location']=ucfirst($donation['location']);
+            $temp['delivery_status']=ucfirst($donation['delivery_status']);
+            $temp['price']=number_format($donation['price'],2);
+            $temp['expiry_in_days']=$donation['expiry_in_days']." Days";
+            $temp['food_type']=ucfirst($donation['food_type']);
+            $temp['event_name']=ucfirst($donation['event_name']);
+            $temp['description']=ucfirst($donation['description']);
+            $temp['prepared_date']=date_format(date_create($donation['prepared_date']),'d-m-Y');
+            $temp['created_at']=date_format(date_create($donation['created_at']),'d-m-Y');
+            $temp['country']=ucfirst($donation['country']);
+            $temp['state']=ucfirst($donation['state']);
+            $temp['city']=ucfirst($donation['city']);
+            $temp['pincode']=$donation['pincode'];
+
+            array_push($donations_list,$temp);
+        }
+        if (count($donations_list)<=0) {
             return response()->json(['message' => 'Donation not found'], 404);
         }
     
-        return response()->json(['donation' => $donation], 200);
+        return response()->json(['donation' => $donations_list], 200);
     }
     
     public function show($id)
