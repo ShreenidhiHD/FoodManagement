848efa1620764e9e54d59b08219ae9b0b9ae9afe
pramod-rkp <99354649+pramod-rkp@users.noreply.github.com>
2023-07-10 00:29:18 +0530

    created functions

--- food-laravel/app/Http/Controllers/PurchaseController.php
+++ food-laravel/app/Http/Controllers/PurchaseController.php
@@ -3,8 +3,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use DB;
+use Illuminate\Support\Facades\Auth;
 
 class PurchaseController extends Controller
 {
-    //
+    //Check if user is authorised to perform operations
+    function is_authorised(){
+        $user = $request->user();
+    }
+
+    //CRUD Operations
+    function create(Request $request){
+        
+    }
+
+    function read(){}
+
+    function update(Request $request){}
+
+    function delete($id){}
 }
